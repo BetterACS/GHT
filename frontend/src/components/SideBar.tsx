49cd981e4add11dddb72473c09152f6668f9eabe
monshinawatra <monshinawatra@gmail.com>
2023-12-09 15:58:09 +0700

    Center containers & fix sidebar animation

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,116 +1,73 @@
-import React, { useState , useEffect } from 'react';
-import { FaHome, FaRegUserCircle } from 'react-icons/fa';
-import { LuSwords } from 'react-icons/lu';
+import React from 'react';
+import clsx from 'clsx';
 import Activity from './Activity';
+import '../styles/Sidebar.css';
+interface BarProps {
+	isOpen: boolean;
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const SideBar = () => {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-	const toggleMenu = () => {
-		setIsMenuOpen(!isMenuOpen);
-	};
-
+const Bar = ({ isOpen, setOpen }: BarProps) => {
 	return (
-		<>
-			<nav className="fixed w-full top-0 left-0 bg-gray-800 mx-auto flex items-center justify-between p-4 z-10">
-				{/* Hamburger Icon (left side) */}
-				<div className="flex items-center">
-					<div className="hamburger">
-						<button
-							onClick={toggleMenu}
-							className={`bg-transparent text-white border-none focus:outline-none transition-all duration-300 ${
-								isMenuOpen ? 'rotate-180' : ''
-							}`}
-						>
-							{isMenuOpen ? (
-								// X Icon
-								<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="2"
-										d="M6 18L18 6M6 6l12 12"
-									></path>
-								</svg>
-							) : (
-								// Hamburger Icon
-								<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="2"
-										d="M4 6h16M4 12h16M4 18h16"
-									></path>
-								</svg>
-							)}
+		<div className="flex flex-row">
+			{/* Sidebar */}
+			<div
+				className={clsx('bg-gray-800 h-screen overflow-y-auto transition-all duration-200', {
+					'w-96': isOpen, // Width when open
+					'w-0': !isOpen, // Width when closed
+				})}
+			>
+				<div className="w-full h-auto p-4 flex flex-row">
+					<div className="basis-10/12 flex flex-row">
+						<img
+							src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t39.30808-6/367450487_1410475112858525_1126649449003258948_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeF2GSPlA0-k5nhSh-LPzQ6OGhcYGNsyLe4aFxgY2zIt7rlUewfPwqtr2HuDgUuPrp9GnFFtlGzr0eWQeJBI8Iya&_nc_ohc=UZr9gBt55oEAX96Knbk&_nc_ht=scontent.fbkk12-4.fna&cb_e2o_trans=t&oh=00_AfBiQ7syIf-XJG9z2-dmAb5Utd8vDj-JjiYS4mWcuofXVw&oe=657949F1"
+							className="profile-sidebar"
+							alt="Pokemon"
+						></img>
+						<div className="text-white self-center px-4">Monshinawatra</div>
+					</div>
+					<button onClick={() => setOpen(!isOpen)}>x</button>
+				</div>
+				{/* Sidebar content goes here */}
+				<div className="w-96">
+					<ul className="p-4">
+						<button className="my-5 p-2 block text-center text-white w-full bg-transparent border-2 border-white focus:outline-none hover:bg-white hover:text-black hover:border-cyan-300 transition-all duration-300">
+							Quest
 						</button>
-					</div>
+						<button className="my-5 p-2 block text-center text-white w-full bg-transparent border-2 border-white focus:outline-none hover:bg-white hover:text-black hover:border-cyan-300 transition-all duration-300">
+							Tasks
+						</button>
+						<button className="my-5 p-2 block text-center text-white w-full bg-transparent border-2 border-white focus:outline-none hover:bg-white hover:text-black hover:border-cyan-300 transition-all duration-300">
+							Achievement
+						</button>
 
-					{/* Home */}
-					<div className="px-6">
-						<a href="/" className="text-white hover:text-gray-400">
-							<FaHome size={25} />
-						</a>
-					</div>
-
-					{/* Sword */}
-					<div className="px-6">
-						<a href="/monster" className="text-white hover:text-gray-400">
-							<LuSwords size={25} />
-						</a>
-					</div>
+						<div className="m-8">
+							<Activity />
+						</div>
+						{/* Add more buttons as needed */}
+					</ul>
 				</div>
+			</div>
 
-				{/* User Authentication or Profile Section */}
-				<div className="px-6">
-					<a href="#" className="text-white hover:text-gray-400">
-						<FaRegUserCircle size={30} />
-					</a>
+			{/* Main content */}
+			<div className="flex-grow">
+				{/* Replace this with your main content */}
+				<div className="w-12 p-4 justify-start">
+					<button
+						className={clsx({ 'opacity-0': isOpen, 'opacity-100': !isOpen })}
+						onClick={() => setOpen(!isOpen)}
+					>
+						y
+					</button>
 				</div>
-			</nav>
-
-            {isMenuOpen && (
-                <DelayedAnimationComponent />
-            )}
-        </>
-    );
+			</div>
+		</div>
+	);
 };
 
-const DelayedAnimationComponent = () => {
-    const [showComponent, setShowComponent] = useState(false);
+const Sidebar = () => {
+	const [open, setOpen] = React.useState(false);
+	return <Bar isOpen={open} setOpen={setOpen} />;
+};
 
-    useEffect(() => {
-        // Delay the animation by 500 milliseconds
-        const timer = setTimeout(() => {
-            setShowComponent(true);
-        }, 0);
-
-        // Clear the timeout when the component unmounts or when isMenuOpen changes
-        return () => clearTimeout(timer);
-    }, []);
-  
-    return (
-      <div className={` min-h-screen top-0 left-0 h-full w-1/6 bg-gray-800 text-black menu-animation ${showComponent ? 'slide-in transition-all duration-300' : 'slide-out'}`}>
-        <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/658_f2.png' className='mt-16' alt='Pokemon'></img>
-        <div className="w-1/2 sm:w-1/8 lg:w-1/2 mx-auto border-t-2 border-white my-2 sm:mb-3 lg:mb-2"></div>
-        <ul className="p-4">
-          <button className="my-5 p-2 block text-center text-white w-full bg-transparent border-2 border-white focus:outline-none hover:bg-white hover:text-black hover:border-cyan-300 transition-all duration-300">
-            Quest
-          </button>
-          <button className="my-5 p-2 block text-center text-white w-full bg-transparent border-2 border-white focus:outline-none hover:bg-white hover:text-black hover:border-cyan-300 transition-all duration-300">
-            Tasks
-          </button>
-          <button className="my-5 p-2 block text-center text-white w-full bg-transparent border-2 border-white focus:outline-none hover:bg-white hover:text-black hover:border-cyan-300 transition-all duration-300">
-            Achievement
-          </button>
-
-          <div className='m-8'>
-            <Activity />
-          </div>
-          {/* Add more buttons as needed */}
-        </ul>
-      </div>
-    );
-  };
-
-export default SideBar;
+export default Sidebar;
