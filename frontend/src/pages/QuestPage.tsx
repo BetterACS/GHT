3844b709dd443876aac33c16617fbc92135f3cc7
JackJessada <jessada.pranee@gmail.com>
2023-12-16 01:16:46 +0700

    update due date

--- frontend/src/pages/QuestPage.tsx
+++ frontend/src/pages/QuestPage.tsx
@@ -29,6 +29,7 @@ import tokenAuth from '../utils/tokenAuth';
 import axios from 'axios';
 import { returnInterface } from '../../../backend/src/utils/interfaces';
 import Config from '../../../backend/src/config';
+import { format } from 'date-fns';
 
 export default function QuestPage() {
 	const navigate = useNavigate();
@@ -831,7 +832,36 @@ export default function QuestPage() {
 							// gainItem(email, newItem);
 						}
 					});
-				}
+					const currentTimestamp = Date.now();
+
+					const formattedDate = new Date(currentTimestamp).toLocaleDateString('en-GB', {
+						year: 'numeric',
+						month: '2-digit',
+						day: '2-digit',
+					}).split("/")
+					console.log(formattedDate);
+					const results = axios.put(
+						`http://localhost:${Config.BACKEND_PORT}/quest`,
+						{
+							quest_id: new_id, //change
+							quest_name: findItemTitle(event.active.id),
+							description: findItemDescription(event.active.id),
+							due_date: formattedDate[2] + "-" + formattedDate[1] + "-" + formattedDate[0],
+							item_id: -1,
+							email: email,
+							status: AfterContainer.title,
+						},
+						{ headers: headers }
+					);
+					const result = (await results).data as returnInterface;
+					authorization(
+						result,
+						async () => {
+							console.log(result.message);
+						},
+						updateAccessToken
+					);
+				}else{
 				// const quest = axios.get(`http://localhost:${Config.BACKEND_PORT}/quest`, {
 				// 	params: {
 				// 		quest_id: new_id, //change
@@ -869,7 +899,7 @@ export default function QuestPage() {
 						console.log(result.message);
 					},
 					updateAccessToken
-				);
+				);}
 			} catch (err) {
 				let container = findValueOfQuest(event.active.id, 'item');
 				container?.items.map((item) => {
